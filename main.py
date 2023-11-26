bc1fa0e2656c9c0a882e6100c9dfb3d9d50d51c2
Ayoub Abidi <mrayoubabidi@gmail.com>
2023-11-26 14:29:14 +0100

    refactor: refactor code into seperate functions

--- main.py
+++ main.py
@@ -11,13 +11,10 @@ topic = "/test"
 
 # Create an MQTT client
 client = mqtt.Client("object_detection_client")
-
-# Connect to the MQTT broker
 client.connect(broker_address)
 
 # Load YOLO model
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-classes = []
 with open("coco.names", "r") as f:
     classes = f.read().strip().split("\n")
 
@@ -33,64 +30,35 @@ fig, ax = plt.subplots()
 # Dictionary to store detection counts for each class
 detection_counts = {"person": 0, "cell phone": 0, "laptop": 0, "book": 0, "chair": 0, "bottle": 0}
 
-while True:
-    ret, frame = cap.read()
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setInput(blob)
-    outs = net.forward(net.getUnconnectedOutLayersNames())
+def process_detection(frame, boxes, confidences, class_ids):
+    for i, (box, confidence, class_id) in enumerate(zip(boxes, confidences, class_ids)):
+        x, y, w, h = box
+        label = str(classes[class_id])
 
-    class_ids = []
-    confidences = []
-    boxes = []
-    width = frame.shape[1]
-    height = frame.shape[0]
+        if label not in ["person", "cell phone"]:
+            continue
 
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+        detection_info = f"{label} - Confidence: {confidence:.2f}"
+        print(detection_info)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        # Publish detection_info to MQTT
+        client.publish(topic, detection_info)
 
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            if label not in ["person", "cell phone"]:
-                continue
-            confidence = confidences[i]
-            detection_info = f"{label} - Confidence: {confidence:.2f}"
-            print(detection_info)
+        # Update detection count for the class
+        detection_counts[label] += 1
 
-            # Publish detection_info to MQTT
-            client.publish(topic, detection_info)
+        # Assign a unique color to each class
+        color = [int(c) for c in class_colors[class_id]]
 
-            # Update detection count for the class
-            detection_counts[label] += 1
+        # Draw rectangle and label on the frame with class-specific color
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-            # Assign a unique color to each class
-            color = [int(c) for c in class_colors[class_ids[i]]]
-
-            # Draw rectangle and label on the frame with class-specific color
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            
-            if label == "cell phone":
-                speech_text = "Please turn off your phone."
-                tts = gTTS(text=speech_text, lang='en')
-                tts.save("warning.mp3")
-                os.system("mpg123 warning.mp3") 
+        if label == "cell phone":
+            speech_text = "Attention, un téléphone a été détecté"
+            tts = gTTS(text=speech_text, lang='fr')
+            tts.save("warning.mp3")
+            os.system("mpg123 warning.mp3")
 
     # Display the frame with object detection results
     cv2.imshow("Object Detection", frame)
@@ -101,6 +69,34 @@ while True:
     ax.bar(labels, counts)
     plt.xticks(rotation='vertical')
     plt.pause(0.01)
+
+# Main loop
+while True:
+    ret, frame = cap.read()
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(net.getUnconnectedOutLayersNames())
+
+    class_ids, confidences, boxes = [], [], []
+    width, height = frame.shape[1], frame.shape[0]
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x, center_y = int(detection[0] * width), int(detection[1] * height)
+                w, h = int(detection[2] * width), int(detection[3] * height)
+                x, y = int(center_x - w / 2), int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    filtered_boxes = [boxes[i] for i in indexes]
+
+    process_detection(frame, filtered_boxes, [confidences[i] for i in indexes], [class_ids[i] for i in indexes])
 
     # Exit on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
